01e5ffc1a213172f747669da1b445d71039629cb
danielnilsson <daniel.nilsson@vinnter.se>
2023-10-19 09:42:40 +0200

    created csv converter class

--- bike_data_scraper/csv_client/csv_handler.py
+++ bike_data_scraper/csv_client/csv_handler.py
@@ -1,39 +1,44 @@
 import csv
 from io import StringIO
 import pandas as pd
+import pytest
 
 
 class UniversalCSVConverter:
-    def __init__(self, columns=None, data=None):
+    # initialize the class with the columns and data
+    def __init__(self, columns, data):
         self.columns = columns
         self.data = data
 
-    def set_data(self, data):
+    def set_data(self, data):  # updates data attribute
         self.data = data
 
-    def set_columns(self, columns):
+    def set_columns(self, columns):  # same as above but with columns
         self.columns = columns
 
-    def to_csv(self):
-        if not self.data:
-            raise ValueError("No data provided")
-
-        if isinstance(self.data, list) and all(
-            isinstance(item, dict) for item in self.data
-        ):
-            return self._dict_list_to_csv(self.data)
-        elif isinstance(self.data, pd.DataFrame):
-            return self.data.to_csv(index=False)
-        else:
-            raise ValueError("Unsupported data type")
-
-    def _dict_list_to_csv(self, items):
+    @staticmethod
+    def convert_to_csv(
+        columns, data
+    ):  # takes columns and data (list of dicts) and returns csv
         csv_file = StringIO()
-        csv_writer = csv.DictWriter(csv_file, fieldnames=self.columns)
+        csv_writer = csv.DictWriter(csv_file, fieldnames=columns)
         csv_writer.writeheader()
-        for item in items:
+        for item in data:
             csv_writer.writerow(item)
 
         csv_data = csv_file.getvalue()
         csv_file.close()
         return csv_data
+
+    def to_csv(self):
+        if self.data is None:  # if data None raise error
+            raise ValueError("No data provided")
+
+        if isinstance(self.data, list) and all(  # if data is a list of dicts
+            isinstance(item, dict) for item in self.data
+        ):
+            return self.convert_to_csv(self.columns, self.data)
+        elif isinstance(self.data, pd.DataFrame):  # if data is a pandas dataframe
+            return self.data.to_csv(index=False)
+        else:
+            raise ValueError("Unsupported data type")
