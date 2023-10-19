a6a7896c2addeb984bc6349b25bafee9aab13416
danielnilsson <daniel.nilsson@vinnter.se>
2023-10-19 15:20:22 +0200

    created class that converts dataframes and json files (list of dicts) to csv

--- bike_data_scraper/csv_client/csv_handler.py
+++ bike_data_scraper/csv_client/csv_handler.py
@@ -1,44 +1,44 @@
 import csv
 from io import StringIO
 import pandas as pd
-import pytest
 
 
 class UniversalCSVConverter:
-    # initialize the class with the columns and data
-    def __init__(self, columns, data):
+    def __init__(self, columns=None, data=None):
         self.columns = columns
         self.data = data
 
-    def set_data(self, data):  # updates data attribute
-        self.data = data
+    def set_data(self, data):
+        self.data = data  # sets
 
-    def set_columns(self, columns):  # same as above but with columns
+    def set_columns(self, columns):
         self.columns = columns
 
     @staticmethod
-    def convert_to_csv(
-        columns, data
-    ):  # takes columns and data (list of dicts) and returns csv
+    def convert_to_csv(columns, data):
         csv_file = StringIO()
         csv_writer = csv.DictWriter(csv_file, fieldnames=columns)
         csv_writer.writeheader()
         for item in data:
             csv_writer.writerow(item)
-
         csv_data = csv_file.getvalue()
         csv_file.close()
         return csv_data
 
-    def to_csv(self):
-        if self.data is None:  # if data None raise error
+    def to_csv(self):  # using instance variables
+        if self.data is None:
             raise ValueError("No data provided")
 
-        if isinstance(self.data, list) and all(  # if data is a list of dicts
+        if isinstance(self.data, pd.DataFrame):
+            return self.data.to_csv(index=False, columns=self.columns)
+
+        elif isinstance(self.data, list) and all(
             isinstance(item, dict) for item in self.data
         ):
-            return self.convert_to_csv(self.columns, self.data)
-        elif isinstance(self.data, pd.DataFrame):  # if data is a pandas dataframe
-            return self.data.to_csv(index=False)
+            columns = self.columns or list(self.data[0].keys())
+            print(self.data[0].keys())
+            print(self.data)
+            return self.convert_to_csv(columns, self.data)
+
         else:
             raise ValueError("Unsupported data type")
