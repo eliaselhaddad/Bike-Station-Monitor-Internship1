70750dca069bb5981f58292269930071d04cf8f1
danielnilsson <daniel.nilsson@vinnter.se>
2023-10-19 16:17:21 +0200

    removed print statement and added comments

--- bike_data_scraper/csv_client/csv_handler.py
+++ bike_data_scraper/csv_client/csv_handler.py
@@ -9,12 +9,14 @@ class UniversalCSVConverter:
         self.data = data
 
     def set_data(self, data):
-        self.data = data  # sets
+        self.data = data  # lets us change the data after instantiation
 
     def set_columns(self, columns):
-        self.columns = columns
+        self.columns = columns  # same as above but for columns
 
-    @staticmethod
+    @staticmethod  # doesn't require self as an argument
+    # can use convert_to_csv without creating an instance of the class
+    # can't modify object state nor class state
     def convert_to_csv(columns, data):
         csv_file = StringIO()
         csv_writer = csv.DictWriter(csv_file, fieldnames=columns)
@@ -25,19 +27,21 @@ class UniversalCSVConverter:
         csv_file.close()
         return csv_data
 
-    def to_csv(self):  # using instance variables
+    def to_csv(self):
         if self.data is None:
             raise ValueError("No data provided")
 
-        if isinstance(self.data, pd.DataFrame):
-            return self.data.to_csv(index=False, columns=self.columns)
+        if isinstance(self.data, pd.DataFrame):  # for pandas dataframes
+            return self.data.to_csv(
+                index=False, columns=self.columns
+            )  # "to_csv" is a pandas method here
 
-        elif isinstance(self.data, list) and all(
+        elif isinstance(self.data, list) and all(  # for lists of dictionaries
             isinstance(item, dict) for item in self.data
         ):
-            columns = self.columns or list(self.data[0].keys())
-            print(self.data[0].keys())
-            print(self.data)
+            columns = self.columns or list(
+                self.data[0].keys()
+            )  # keys in this case are the column names
             return self.convert_to_csv(columns, self.data)
 
         else:
