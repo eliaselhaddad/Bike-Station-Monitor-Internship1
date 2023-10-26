9882580e73b0c81909e2cefae0d3543185c7e4e8
Erik <erik.manfredsson@vinnter.se>
2023-10-26 16:07:32 +0200

    Moved dependencies to dev, cleaned up step functions construct a bit

--- infrastructure/constructs/step_functions_poc.py
+++ infrastructure/constructs/step_functions_poc.py
@@ -67,12 +67,10 @@ class StepFunctionsPoc(Construct):
             function_name=f"{stage_name}-{service_short_name}-graphs-data-scraper",
         )
 
-        lambda_role_name = f"{weather_lambda}-role"
-        self.lambda_role = self._build_lambda_role(role_name=lambda_role_name)
-
         first_parallel_lambda_task = tasks.LambdaInvoke(
             self, id="FetchBikeDataFromApi", lambda_function=bike_lambda
         )
+
         second_parallel_lambda_task = tasks.LambdaInvoke(
             self,
             id="FetchWeatherDataFromApi",
@@ -98,38 +96,3 @@ class StepFunctionsPoc(Construct):
         fourth_lambda_task.next(fifth_lambda_task)
 
         sfn.StateMachine(self, id="MyStepMachinesId", definition=parallel_task)
-
-    def _build_lambda(
-        self,
-        lambda_name: str,
-        stage_name: str,
-        lambda_path: str,
-        cwd: str = os.getcwd(),
-    ):
-        lambda_function = aws_lambda.Function(
-            self,
-            lambda_name,
-            function_name=lambda_name,
-            runtime=aws_lambda.Runtime.PYTHON_3_10,
-            code=aws_lambda.Code.from_asset(os.path.join(cwd, ".build/lambdas/")),
-            handler=lambda_path,
-            tracing=aws_lambda.Tracing.ACTIVE,
-            retry_attempts=2,
-            timeout=Duration.seconds(80),
-            memory_size=128,
-            role=self.lambda_role,
-            layers=[self.lambda_layer],
-        )
-        return lambda_function
-
-    def _build_lambda_role(self, role_name: str) -> aws_iam.Role:
-        return aws_iam.Role(
-            self,
-            role_name,
-            assumed_by=aws_iam.ServicePrincipal("lambda.amazonaws.com"),
-            managed_policies=[
-                aws_iam.ManagedPolicy.from_aws_managed_policy_name(
-                    managed_policy_name="service-role/AWSLambdaBasicExecutionRole"
-                )
-            ],
-        )
